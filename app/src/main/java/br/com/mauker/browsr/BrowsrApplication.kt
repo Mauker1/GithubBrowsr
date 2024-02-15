396f296a85f0a50c45bca638fe8ac1696cd767b0
Mauricio Pessoa <mauricio.pessoa@mimi.io>
2024-02-15 13:13:51 +0100

    Structuring DI for application

---
+++ app/src/main/java/br/com/mauker/browsr/BrowsrApplication.kt
@@ -0,0 +1,4 @@
+package br.com.mauker.browsr
+
+class BrowsrApplication {
+}
---
+++ app/src/main/java/br/com/mauker/browsr/MainViewModel.kt
@@ -0,0 +1,10 @@
+package br.com.mauker.browsr
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    fun foo() {
+        // Do something
+    }
+}
